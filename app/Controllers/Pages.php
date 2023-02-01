ba203f6a256b8b5fca3f9f02cdc43869777e31f5
Dominique Foley <dfoleydev@gmail.com>
2023-02-01 17:41:20 -0500

    Refactoring controller code

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -15,9 +15,11 @@ class Pages extends BaseController
         if (!is_file(APPPATH . '/Views/pages/' . $page . '.php')) {
             throw new PageNotFoundException($page);
         }
+
         $data['title'] = ucfirst($page);
-        echo view('templates/header', $data);
-        echo view('pages/' . $page,);
-        echo view('templates/footer');
+
+        return view('templates/header', $data)
+            . view('pages/' . $page,)
+            . view('templates/footer');
     }
 }
