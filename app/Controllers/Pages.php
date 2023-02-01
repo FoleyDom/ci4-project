84da03fe24e31dce7ccc9f2fadf76fa4b6726f17
Dominique Foley <dfoleydev@gmail.com>
2023-02-01 11:19:34 -0500

    Controller created, page is working

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function view($page = 'home')
+    {
+        //
+    }
+}
---
+++ app/Views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+
+<body>
+    <h1><?= esc($title) ?></h1>
+</body>
+
+</html>
